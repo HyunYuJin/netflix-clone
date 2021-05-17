efe8fe5aa9cadecb038be99e90f4fabcb0496cad
현유진 <gusdbwls9772@gmail.com>
2021-05-17 23:32:51 +0900

    GET data, Developing slider

--- src/js/api/index.js
+++ src/js/api/index.js
@@ -1,21 +1,22 @@
-export const API_URL = 'https://api.themoviedb.org/3';
-export const API_KEY = '0e6d860fdb93125a12911f42a73dd701'
+const API_URL = 'https://api.themoviedb.org/3';
+const API_KEY = '0e6d860fdb93125a12911f42a73dd701'
+const BASE_IMAGE_URL = 'https://image.tmdb.org/t/p/w500/'
 
-// https://api.themoviedb.org/3/movie/popular?
-// /movie/popular
-export function requestPopular() {
-    fetch(API_URL + '/movie/popular?api_key=' + API_KEY)
-        .then(res => {
-            return res.json();
-        }).then(data => {
-            const result = data.results.sort((a, b) => {
-                return b.popularity - a.popularity
-            })
+export const tmdb = {
+    API_URL,
+    API_KEY,
+    BASE_IMAGE_URL,
 
-            return result
-        }).catch(err => {
-            console.log('Fetch Error', err);
-        });
+    // https://api.themoviedb.org/3/movie/popular?
+    getPopularMovie () {
+        return window.fetch(`${API_URL}/movie/popular?api_key=${API_KEY}`)
+            .then(res => res.json())
+    },
+
+    // https://api.themoviedb.org/3/movie/{id}?
+    getVideo(id) {
+        return window.fetch(`${API_URL}/movie/${id}/videos?api_key=${API_KEY}`)
+            .then(res => res.json())
+    }
 }
 
-// https://api.themoviedb.org/3/movie/550?api_key=0e6d860fdb93125a12911f42a73dd701
--- src/js/pages/home.js
+++ src/js/pages/home.js
@@ -1,6 +1,6 @@
 import View from './view'
 import template from '../../components/home.html'
-import { API_URL, API_KEY } from '../api/index'
+import { tmdb } from '../api/index'
 
 class Home extends View {
     constructor() {
@@ -9,21 +9,17 @@ class Home extends View {
             className: 'home'
         }) // 이걸로 부모에 접근할 수 있다. (부모 클래스 constructor에 접근하도록 한다.)
 
-        this.$slidContainer = this.$element.querySelector('.slide-container')
-        this.$slider = this.$element.querySelector('.slider')
+        this.$mainBanner = this.$element.querySelector('.mainBanner')
+        this.$slideList = this.$element.querySelector('.slide-list')
+        this.$slideContents = null // 모든 item
         this.$prevBtn = this.$element.querySelector('.prevBtn')
         this.$nextBtn = this.$element.querySelector('.nextBtn')
-        this.slideCount = 0
-        this.currentIndex = 0
-
-        this.totalSlider = null
-        this.totalLength = 0
-        this.slideWidth = 400
+        this.slideWidth = 16.6666667
+        this.slideLength = 0
     }
 
     created() {
         console.log('created')
-        
         this._requestPopular()
     }
 
@@ -35,36 +31,117 @@ class Home extends View {
         console.log('destroyed')
     }
 
-    // 영화 인기 순위를 불러오는 API
+    // Youtube에서 제공하는 영화 예고편 요청
+    _requestVedio(id) {
+        const VIDEO_URL = 'https://www.youtube.com/embed/'
+        tmdb.getVideo(id)
+        .then(data => {
+            if (data.results.length > 0) {
+                const youtubeId = data.results[0].key
+                this.$mainBanner.innerHTML += `<iframe width="100%" height="100%" src="${VIDEO_URL + youtubeId}?autoplay=1"></iframe>`; 
+            } else {
+                this.$mainBanner.innerHTML += `<div>재생할 예고편이 없습니다.</div>`; 
+            }
+        })
+        .catch(error => {
+            console.log(error)
+        })
+    }
+
+    // 영화 인기 순위 API
     _requestPopular() {
-        fetch(API_URL + '/movie/popular?api_key=' + API_KEY)
-        .then(res => {
-            return res.json()
-        }).then(data => {
-            const result = data.results.sort((a, b) => {
+        tmdb.getPopularMovie()
+        .then(data => {
+            // 인기가 많은 순서대로 정렬
+            const movieList = data.results.sort((a, b) => {
                 return b.popularity - a.popularity
             })
 
-            this.totalSlider = result
-            
-            for (let i = 0; i < this.totalSlider.length; i++) {
+            // movieList.forEach((key) => {
+            //     this._requestVedio(key.id)
+            // })
+
+            // this.$slideList.style.width = this.slideWidth * movieList.length + 'px'
+            this.$slideList.style.width = this.slideWidth * movieList.length + '%'
+
+            movieList.forEach((movie) => {
                 const $sliderItem = document.createElement('div')
-                $sliderItem.className = 'slider-item'
-                this.$slider.appendChild($sliderItem)
+                $sliderItem.className = 'slide-content'
+                $sliderItem.innerHTML = `<img src=${tmdb.BASE_IMAGE_URL + movie.backdrop_path} />`
+                // $sliderItem.style.backgroundImage = `url('${BASE_IMAGE_URL + movie.backdrop_path}')`
+                this.$slideList.appendChild($sliderItem)
+                $sliderItem.setAttribute('data-id', movie.id)
 
-                $sliderItem.style.width = this.slideWidth + 'px'
-                $sliderItem.style.backgroundImage = `url('https://image.tmdb.org/t/p/w500/${this.totalSlider[i].backdrop_path}')`;
-            }
+                // 마우스 hover 했을 때 영화에 대한 자세한 정보를 담고 있는 부분
+                const $sliderItemDetail = document.createElement('div')
+                $sliderItemDetail.className = 'slider-detail'
+                $sliderItem.appendChild($sliderItemDetail)
+
+                // original_title, overview, release_date
+                const movieTitle = document.createElement('h4')
+                const movieOverview = document.createElement('p')
+                const movieReleaseDate = document.createElement('span')
+
+                movieTitle.innerHTML = movie.original_title
+                movieOverview.innerHTML = movie.overview
+                movieReleaseDate.innerHTML = movie.release_date
+
+                $sliderItemDetail.appendChild(movieTitle)
+                $sliderItemDetail.appendChild(movieOverview)
+                $sliderItemDetail.appendChild(movieReleaseDate)
+
+                $sliderItem.addEventListener('mouseenter', () => {
+                    $sliderItemDetail.classList.add('hover')
+                })
+                $sliderItem.addEventListener('mouseleave', () => {
+                    $sliderItemDetail.classList.remove('hover')
+                })
+            })
             
-            const $slideContents = document.querySelectorAll('.slider-item')
-            this.slideCount = $slideContents.length
-            this.$slider.style.width = this.slideWidth * (slideLen) + 'px'
+            this.$slideContents = document.querySelectorAll('.slide-content')
+            const slideLength = this.$slideContents.length
 
+            let currentIndex = 0
+            if (currentIndex === 0) {
+                this.$prevBtn.classList.add('hide')
+            }
 
-        }).catch(err => {
-            console.log('Fetch Error', err)
-        });
+            // next Button
+            this.$nextBtn.addEventListener('click', () => {
+                if (currentIndex <= slideLength - 1) {
+                    this.$prevBtn.classList.remove('hide')
+                    // this.$slideList.style.transform = 'translate3d(-' + (this.slideWidth * (currentIndex + 1)) + 'px, 0px, 0px)'
+                    this.$slideList.style.transform = 'translate3d(-' + (5 * (currentIndex + 1)) + '%, 0px, 0px)'
+                }
+                
+                if (currentIndex == slideLength - 2) {
+                    currentIndex = 0
+                } else {
+                    currentIndex++
+                }
 
+                console.log(currentIndex, slideLength - 1)
+            })
+            
+            // prev Button
+            this.$prevBtn.addEventListener('click', () => {
+                if (currentIndex > 0) {
+                    // this.$slideList.style.transform = 'translate3d(-' + (this.slideWidth * (currentIndex - 1)) + 'px, 0px, 0px)'
+                    this.$slideList.style.transform = 'translate3d(-' + (5 * (currentIndex - 1)) + '%, 0px, 0px)'
+                }
+                
+                if (currentIndex == 0) {
+                    currentIndex = slideLength - 1
+                } else {
+                    currentIndex--
+                }
+                
+                console.log(currentIndex, slideLength - 1)
+            })
+        })
+        .catch(err => {
+            console.log('Fetch Error', err);
+        })
     }
 }
 
